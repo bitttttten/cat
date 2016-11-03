301f335b22900ca2d59866eb8edfef0ddcb3d48e
bitttttten <git@aharding.co.uk>
2016-11-03 22:33:34 +0100

    moved eslint out and updated configs to be ignored

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -35,7 +35,7 @@ module.exports = {
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production') // eslint-disable-line
+        'NODE_ENV': JSON.stringify('production')
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
